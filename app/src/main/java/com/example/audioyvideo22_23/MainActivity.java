cd12b304395cb82f11ed619c29f5404d2816035c
manuel <mpgprofe@iesflorenciopintado.es>
2022-10-20 11:07:25 +0200

    Reproduciendo un vídeo y un seekBar va con la canción.

--- app/src/main/java/com/example/audioyvideo22_23/MainActivity.java
+++ app/src/main/java/com/example/audioyvideo22_23/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.audioyvideo22_23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import java.io.IOException;
 
@@ -14,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     Button buttonPlay, buttonStop, buttonPause;
     MediaPlayer mediaPlayer;
     TextView textViewEstado;
+    VideoView videoView;
+    MediaController mediaController;
+    BarraMusica barraMusica=null;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,27 @@ public class MainActivity extends AppCompatActivity {
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonStop = findViewById(R.id.buttonStop);
         textViewEstado = findViewById(R.id.textViewEstado);
+        videoView = findViewById(R.id.videoView);
+        seekBar = findViewById(R.id.seekBar);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.musica);
+        mediaPlayer = MediaPlayer.create(this, R.raw.musica); //Para el audio
+
+        //Para el vídeo
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        String uri = "android.resource://" + getPackageName() + "/" + R.raw.magia;
+        videoView.setVideoURI(Uri.parse(uri));
+        videoView.setMediaController(mediaController);
+        //videoView.start(); Si quiero que empiece automáticamente debo asegurarme de que está el vídeo preparado
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                videoView.start();
+                mediaController.show();
+            }
+        });
+
 
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
                 if (mediaPlayer.isPlaying()) {
                     textViewEstado.setText("Ya está sonando");
                 } else {
+                    if (barraMusica==null){
+                        barraMusica = new BarraMusica(seekBar,mediaPlayer);
+                        barraMusica.execute();
+                    }
                     mediaPlayer.start();
                     textViewEstado.setText("La canción está sonando");
                 }
@@ -41,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mediaPlayer.isPlaying()){
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                     textViewEstado.setText("Canción en STOP");
                     try {
@@ -49,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }else{
+                } else {
                     textViewEstado.setText("Ninguna canción sonando");
                 }
             }
@@ -57,15 +90,51 @@ public class MainActivity extends AppCompatActivity {
         buttonPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer.isPlaying()){
+                if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
                     textViewEstado.setText("Pausada");
-                }else{
+                } else {
                     textViewEstado.setText("Ninguna canción suena");
                 }
             }
         });
 
 
+    }
+
+    class BarraMusica extends AsyncTask<String, String, String> {
+        SeekBar miSeekBar;
+        MediaPlayer miMediaPlayer;
+
+        public BarraMusica(SeekBar miSeekBar, MediaPlayer miMediaPlayer) {
+            this.miSeekBar = miSeekBar;
+            this.miMediaPlayer = miMediaPlayer;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            miSeekBar.setMax(miMediaPlayer.getDuration());
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            miSeekBar.setProgress(miMediaPlayer.getCurrentPosition());
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            while (true) {
+                if (miMediaPlayer.isPlaying()) {
+                    publishProgress();
+                }
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 }
